72534fe1678916dfd02ae1109e9b86c3ed9e71c4
marcos21009 <marco21.009@gmail.com>
2020-10-15 22:25:19 -0500

    Initial Commit

---
+++ src/Stylist/Theme/Exceptions/ThemeJsonNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tecnodesignc\Stylist\Theme\Exceptions;
+
+class ThemeJsonNotFoundException extends \Exception
+{
+	public function __construct($path)
+    {
+        $this->message = "theme.json file does not exist at [$path].";
+    }
+}
